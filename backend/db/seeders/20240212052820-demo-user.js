d37a445c74860c85c2a7200187f17bcfd786a663
Sean <Kwasnicki.Sean@gmail.com>
2024-02-14 15:38:41 -0800

    adding first and last name to seeder file

--- backend/db/seeders/20240212052820-demo-user.js
+++ backend/db/seeders/20240212052820-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'User',
+        lastName: 'Tester',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'User',
+        lastName: 'Tester',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'User',
+        lastName: 'Tester',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
@@ -37,4 +43,3 @@ module.exports = {
     }, {});
   }
 };
-
