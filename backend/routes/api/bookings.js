08c035e561e03f6a21a86524d52861da48d81552
Sean-Kwasnicki <kwasnicki.sean@gmail.com>
2024-02-29 17:57:06 -0800

    debugging

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -107,18 +107,18 @@ router.put('/:bookingId', requireAuth, validateBooking, async (req, res) => {
   const userId = req.user.id;
   const { bookingId } = req.params;
   const { startDate, endDate } = req.body;
-  const now = new Date();
+  //const now = new Date();
   try {
     const booking = await Booking.findByPk(bookingId);
-    if (!booking) {
-      return res.status(404).json({ message: "Booking couldn't be found" });
-    }
+    // if (!booking) {
+    //   return res.status(404).json({ message: "Booking couldn't be found" });
+    // }
     // if (new Date(booking.endDate) < now) {
     //   return res.status(403).json({ message: "Past bookings can't be modified" });
     // }
-    if (booking.userId !== userId) {
-      return res.status(403).json({ message: "You don't have permission to edit this booking" });
-    }
+    // if (booking.userId !== userId) {
+    //   return res.status(403).json({ message: "You don't have permission to edit this booking" });
+    // }
 
     const conflictingBookings = await Booking.findAll({
       where: {
